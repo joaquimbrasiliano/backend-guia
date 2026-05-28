af079ce9d55e934528c92a50ba88d7ae2624e127
joaquimbrasiliano <joaquimbrasilianog@gmail.com>
2026-05-28 10:13:50 -0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
+import os
 
 class Settings(BaseSettings):
-    gemini_api_key: str = ""
+    gemini_api_key: str = os.getenv('GEMINI_API_KEY')
     ocr_confidence_threshold: float = 0.6
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
